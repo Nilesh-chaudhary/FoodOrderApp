68c8b09fc6cb3913904f48114382640723136f6e
Nilesh-chaudhary <nileshchaudhary532@gmail.com>
2023-03-17 23:48:23 +0530

    added store for using context api for items to be added into cart

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
-import { Fragment } from "react";
-
 import { useState } from "react";
 import Header from "./Components/Layout/Header";
 import Meals from "./Components/Meals/Meals";
 import Cart from "./Components/Cart/Cart";
+import CartProvider from "./store/CartProvider";
 
 function App() {
   const [cartIsShown, setCartIsShown] = useState(false);
@@ -17,13 +16,13 @@ function App() {
   };
 
   return (
-    <Fragment>
+    <CartProvider>
       {cartIsShown && <Cart onClose={hideCartHandler} />}
       <Header onShowCart={showCartHandler} />
       <main>
         <Meals />
       </main>
-    </Fragment>
+    </CartProvider>
   );
 }
 
